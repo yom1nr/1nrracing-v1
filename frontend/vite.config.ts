e4bd7776526d154bf2131f6106691c4ec5782d0e
yom1nr <hotest224e@gmail.com>
2025-09-10 02:31:16 +0700

    Deploy frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// เปลี่ยนชื่อ repository ของคุณถ้าไม่ได้ใช้ root domain
-const repoName = '1nrracing0.1'  
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // important สำหรับ GitHub Pages
-  build: {
-    outDir: 'dist',       // จะสร้าง dist/ ใน frontend
-    emptyOutDir: true     // ลบไฟล์เก่าก่อน build
-  },
-  server: {
-    port: 5173
-  }
+  server: { port: 5173 }
 })
